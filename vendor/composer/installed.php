fe3fd3dfedbaf238c36f08e42938b6765dc769e8
tiago <tiagofeli.rk@gmail.com>
2021-03-09 15:01:38 -0300

    inicio  do projeto

---
+++ vendor/composer/installed.php
@@ -0,0 +1,51 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'da4933f0ff20c8ecfe1b6c7d03e7d4e6e2427c44',
+    'name' => 'hcodebr/ecommerce',
+  ),
+  'versions' => 
+  array (
+    'hcodebr/ecommerce' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'da4933f0ff20c8ecfe1b6c7d03e7d4e6e2427c44',
+    ),
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'v5.2.22',
+      'version' => '5.2.22.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b18cb98131bd83103ccb26a888fdfe3177b8a663',
+    ),
+    'rain/raintpl' => 
+    array (
+      'pretty_version' => '3.0.0',
+      'version' => '3.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5bfdf4602b8adb0e80b222fa29ac559aa9c06abf',
+    ),
+    'slim/slim' => 
+    array (
+      'pretty_version' => '2.0.0',
+      'version' => '2.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3e95c9abbc57a8f023ab4dacebcab9dae4d9f1f0',
+    ),
+  ),
+);
